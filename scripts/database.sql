e26631254b494f573aa1f35d4c66d4ec65e7c0d0
Ahmed Ashraf Hamdy <ahm.ashraf22@gmail.com>
2025-10-02 00:54:13 +0300

    Create database.sql

---
+++ scripts/database.sql
@@ -0,0 +1,14 @@
+-- Creating Database 'DataWarehouse'
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+-- Creating Schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
