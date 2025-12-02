86106dbcefb4ca22dba64fae63dc4ab65c5e1876
Ahmed Ashraf Hamdy <ahm.ashraf22@gmail.com>
2025-12-02 02:42:09 +0200

    Update loading_data_silver.sql

--- scripts/silver/loading_data_silver.sql
+++ scripts/silver/loading_data_silver.sql
@@ -65,3 +65,64 @@ CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) -1 AS D
  The leading start date minus one as en end date, also we removed the time as it's useless */ 
 FROM bronze.crm_prd_info;
 SELECT * FROM silver.crm_prd_info
+
+--------------
+
+-- silver.crm_sales_details
+
+INSERT INTO silver.crm_sales_details (
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+)
+
+SELECT 
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+	 ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+END sls_order_dt,
+CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+	 ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+END AS sls_ship_dt,
+CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+	 ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+END AS sls_due_dt,
+CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+		THEN sls_quantity * ABS(sls_price)
+	 ELSE sls_sales
+END AS sls_sales,
+sls_quantity,
+CASE WHEN sls_price is NULL or sls_price <= 0
+		THEN sls_sales / NULLIF(sls_quantity, 0)
+	 ELSE sls_price
+END AS sls_price
+FROM bronze.crm_sales_details;
+
+-- silver.erp_cust_az12
+
+INSERT INTO silver.erp_cust_az12 (
+	cid,
+	bdate,
+	gen
+)
+
+SELECT 
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+	 ELSE cid 
+END AS cid,
+CASE WHEN bdate > GETDATE() THEN NULL
+	ELSE bdate
+END as bdate,
+CASE WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+	 WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+	ELSE 'N/A'
+END as gen
+FROM bronze.erp_cust_az12;
